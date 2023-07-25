92b18c7e68213e0d968509cd3058f9ee58d0a3e7
Whitley Horn <whitleyblaine@protonmail.com>
2023-07-25 01:41:31 -0400

    Add nvim-treesitter

--- plugins.lua
+++ plugins.lua
@@ -58,5 +58,23 @@ local plugins = {
       }
     },
   },
-}
-return plugins
+  {
+        "nvim-treesitter/nvim-treesitter",
+        opts = {
+          ensure_installed = {
+            -- defaults
+            "vim",
+            "lua",
+
+            -- web dev
+            "html",
+            "css",
+            "javascript",
+            "typescript",
+            "tsx",
+            "json",
+          },
+        },
+      },
+    }
+    return plugins
