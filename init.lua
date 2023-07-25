4951ba94e3f33404011f39ce8bcaf236c75ea0b7
Whitley Horn <whitleyblaine@protonmail.com>
2023-07-24 20:45:10 -0400

    Add Copilot, customize nvim-cmp, add custom luasnip snippets for typescript

---
+++ init.lua
@@ -0,0 +1,5 @@
+-- Set the path to your snippets directory
+vim.g.vscode_snippets_path = '~/.config/nvim/lua/custom/my_snippets'
+
+-- Copilot
+vim.g.copilot_assume_mapped = true
---
+++ plugins.lua
@@ -0,0 +1,62 @@
+local overrides = require("custom.configs.overrides")
+local cmp = require "cmp"
+
+local plugins = {
+  {
+    "jackmort/ChatGPT.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim"
+    },
+    config = function()
+      require("chatgpt").setup({
+        api_key_cmd = "pass show api/tokens/openai",
+      })
+    end,
+  },
+  {
+    "zbirenbaum/copilot.lua",
+    event = "InsertEnter",
+    opts = overrides.copilot,
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      {
+        "zbirenbaum/copilot-cmp",
+        config = function()
+          require("copilot_cmp").setup()
+        end,
+      },
+    },
+    opts = {
+      sources = {
+        { name = "nvim_lsp", group_index = 2 },
+        { name = "copilot",  group_index = 2 },
+        { name = "luasnip",  group_index = 2 },
+        { name = "buffer",   group_index = 2 },
+        { name = "nvim_lua", group_index = 2 },
+        { name = "path",     group_index = 2 },
+      },
+      mapping = {
+        ["<Up>"] = cmp.mapping.select_prev_item(),
+        ["<Down>"] = cmp.mapping.select_next_item(),
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true })  -- added line
+          elseif require("luasnip").expand_or_jumpable() then
+            vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
+          else
+            fallback()
+          end
+        end, {
+          "i",
+          "s",
+        }),
+      }
+    },
+  },
+}
+return plugins
